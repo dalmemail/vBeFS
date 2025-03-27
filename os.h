79011907058eb6feedf20d6e5bcb80eaaf4ddfb5
Daniel Martín <dalmemail@gmail.com>
2025-03-27 22:57:42 +0100

    Still useless, but at least verifies now

---
+++ os.h
@@ -0,0 +1,4 @@
+// The frontend should define these types
+typedef int int32;
+typedef unsigned int uint32;
+typedef unsigned long int off_t;
--- superblock.h
+++ superblock.h
@@ -1,3 +1,22 @@
+#include "os.h"
+
+#define SUPER_BLOCK_MAGIC1 0x42465331 /* BFS1 */
+#define SUPER_BLOCK_MAGIC2 0xdd121031
+#define SUPER_BLOCK_MAGIC3 0x15b6830e
+
+#define BFS_DISK_NAME_LENGTH 32
+
+#define SUPER_BLOCK_FS_LITTLE_ENDIAN 0x42494745 /* BIGE */
+#define SUPER_BLOCK_DISK_CLEAN 0x434c454e /* CLEN */
+
+struct vbefs_block_run
+{
+};
+
+struct vbefs_inode_addr
+{
+};
+
 struct vbefs_superblock
 {
 	char name[BFS_DISK_NAME_LENGTH];
@@ -13,16 +32,18 @@ struct vbefs_superblock
 	int32 ag_shift;
 	int32 num_ags;
 	int32 flags;
-	block_run log_blocks;
+	struct vbefs_block_run log_blocks;
 	off_t log_start;
 	off_t log_end;
 	int32 magic3;
-	inode_addr root_dir;
-	inode_addr indices;
+	struct vbefs_inode_addr root_dir;
+	struct vbefs_inode_addr indices;
 	int32 pad[8];
 };
 
 /*@
+    predicate inode(struct vbefs_inode_addr inode_addr) = true;
+    predicate block_run(struct vbefs_block_run block_run, off_t start, off_t end) = true;
 	// is n == 2^exp ?
 	// TODO: This can probably be done in a more "idiomatic" way somehow
 	predicate power_of_two(uint32 n, uint32 exp) = (exp == 0) ? n == 1 : (n % 2 == 0 &*& power_of_two(n / 2, exp - 1));
@@ -32,7 +53,7 @@ struct vbefs_superblock
 		superblock->magic1 |-> ?magic1 &*& magic1 == SUPER_BLOCK_MAGIC1 &*&
 		superblock->fs_byte_order |-> ?fs_byte_order &*& fs_byte_order == SUPER_BLOCK_FS_LITTLE_ENDIAN &*&
 		superblock->block_size |-> ?block_size &*&
-		superblock->block_shift |-> ?block_shift &*& block_size == power_of_two(block_shift) &*&
+		superblock->block_shift |-> ?block_shift &*& power_of_two(block_size, block_shift) &*&
 		superblock->num_blocks |-> ?num_blocks &*&
 		superblock->used_blocks |-> ?used_blocks &*& used_blocks <= num_blocks &*&
 		superblock->inode_size |-> ?inode_size &*&
@@ -45,16 +66,7 @@ struct vbefs_superblock
 		superblock->log_start |-> ?log_start &*&
 		superblock->log_end |-> ?log_end &*& block_run(log_blocks, log_start, log_end) &*&
 		superblock->magic3 |-> ?magic3 &*& magic3 == SUPER_BLOCK_MAGIC3 &*&
-		superblock->root_dir |-> ?root_dir &*& block_run(root_dir) &*&
-		superblock->indices |-> ?indices &*& block_run(indices) &*&
+		superblock->root_dir |-> ?root_dir &*& inode(root_dir) &*&
+		superblock->indices |-> ?indices &*& inode(indices) &*&
 		superblock->pad |-> _;
 @*/
-
-#define SUPER_BLOCK_MAGIC1 0x42465331 /* BFS1 */
-#define SUPER_BLOCK_MAGIC2 0xdd121031
-#define SUPER_BLOCK_MAGIC3 0x15b6830e
-
-#define BFS_DISK_NAME_LENGTH 32
-
-#define SUPER_BLOCK_FS_LITTLE_ENDIAN 'BIGE' /* BIGE */
-#define SUPER_BLOCK_DISK_CLEAN 'CLEN' /* CLEN */
